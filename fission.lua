8b602ca81ef2cd8fdd75b5891d9d5f837b4a0a20
0mori1 <117737361+0mori1@users.noreply.github.com>
2024-03-06 10:01:39 +0900

    Update fission.lua

--- fission.lua
+++ fission.lua
@@ -22,16 +22,28 @@ gpu.bind(screen)
 local xr, yr = gpu.getResolution()
 gpu.fill(1, 1, xr, yr, " ")
 local function text(x, y, color, text)
+    text = text or "Hello, world!"
     gpu.setForeground(color or 0xFFFFFF)
-    gpu.set(x or 1, y or 1, text or "Hello, world!")
+    gpu.set(x or 1, y or 1, text)
+    local metElement = {}
+    function metElement:remove()
+        local emptyString = ""
+        for i = 1,#text,1 do
+            emptyString = emptyString .. " "
+        end
+        gpu.set(x or 1, y or 1, emptyString)
+    end
+    return metElement
 end
-text(1, 4, 0xFFFF00, "Heat: "..tostring(reactor.getHeatLevel()) .. "/" .. tostring(reactor.getMaxHeatLevel() .. " H"))
-text(1, 5, 0x00FF00, "Power: " .. tostring(reactor.getEnergyStored()) .. "/"..tostring(reactor.getMaxEnergyStored()).. " RF")
+heat = text(1, 4, 0xFFFF00, "Heat: "..tostring(reactor.getHeatLevel()) .. "/" .. tostring(reactor.getMaxHeatLevel() .. " H"))
+power = text(1, 5, 0x00FF00, "Power: " .. tostring(reactor.getEnergyStored()) .. "/"..tostring(reactor.getMaxEnergyStored()).. " RF")
 while true do
     os.sleep(0.1)
     gpu.setBackground(0x000000)
-    text(1, 4, 0xFFFF00, "Heat: "..tostring(reactor.getHeatLevel()) .. "/" .. tostring(reactor.getMaxHeatLevel()) .. " H")
-    text(1, 5, 0x00FF00, "Power: " .. tostring(reactor.getEnergyStored()).. "/"..tostring(reactor.getMaxEnergyStored()).. " RF")
+    heat:remove()
+    power:remove()
+    heat = text(1, 4, 0xFFFF00, "Heat: "..tostring(reactor.getHeatLevel()) .. "/" .. tostring(reactor.getMaxHeatLevel()) .. " H")
+    power = text(1, 5, 0x00FF00, "Power: " .. tostring(reactor.getEnergyStored()).. "/"..tostring(reactor.getMaxEnergyStored()).. " RF")
     ratio = reactor.getHeatLevel() / reactor.getMaxHeatLevel()
     energyratio = reactor.getEnergyStored() / reactor.getMaxEnergyStored()
     if ratio >= 0.5 or energyratio >= 0.9 then
